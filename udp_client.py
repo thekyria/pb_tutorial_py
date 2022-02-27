ce02b206c7b449afcc770bae2862b943080843d5
thekyria <thekyria@gmail.com>
2022-02-27 15:12:09 +0200

    adding argparse

--- udp_client.py
+++ udp_client.py
@@ -1,30 +1,51 @@
 #!/usr/bin/env python
 
+import argparse
 import socket
 from simple_message import simple_message_pb2, simple_message_utils
 
-buffer_size = 1024
-source_port = 20212
-target_ip = "192.168.1.60"
-target_port = 20060
+DEFAULT_BUFFER_SIZE = 1024
+DEFAULT_LOCAL_PORT = 20212
+DEFAULT_TIMEOUT = 10.0
 
 
-def main():
-    print("udp_client")
+def read_cmd_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--local-port", type=int, default=DEFAULT_LOCAL_PORT,
+                        help="Port where the UDP packet will be sent from")
 
-    rx_tx_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-    rx_tx_socket.bind(('', source_port))
+    parser.add_argument("-a", "--target-ip", type=str, required=True,
+                        help="Target destination IP address")
+    parser.add_argument("-p", "--target-port", type=int, required=True,
+                        help="Target destination port")
 
-    message_tx = simple_message_pb2.simple_message()
-    message_tx.opcode = 11
-    message_tx.payload = "this is my payload"
-    message_tx.crc32 = 1234
-    print(f"tx ({target_ip}:{target_port}): {simple_message_utils.simple_message_to_str(message_tx)}")
-    rx_tx_socket.sendto(message_tx.SerializeToString(), (target_ip, target_port))
+    parser.add_argument("-b", "--buffer-size", type=int, default=DEFAULT_BUFFER_SIZE,
+                        help="Size of the internal rx/tx buffer in bytes")
+    args = parser.parse_args()
+    return args.local_port, args.target_ip, args.target_port, args.buffer_size
 
-    message_rx, address = rx_tx_socket.recvfrom(buffer_size)
-    print(f"rx ({address[0]}:{address[1]}): {message_rx.decode()}")
+
+def main(_local_port: int, _target_ip: str, _target_port: int, _buffer_size: int):
+    print(f"udp_client[{_buffer_size}] :{_local_port} -> {_target_ip}:{_target_port}")
+
+    with socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM) as rx_tx_socket:
+        rx_tx_socket.settimeout(DEFAULT_TIMEOUT)
+        rx_tx_socket.bind(('', _local_port))
+
+        message_tx = simple_message_pb2.simple_message()
+        message_tx.opcode = 11
+        message_tx.payload = "this is my payload"
+        message_tx.crc32 = 1234
+        print(f"tx ({_target_ip}:{_target_port}): {simple_message_utils.simple_message_to_str(message_tx)}")
+        rx_tx_socket.sendto(message_tx.SerializeToString(), (_target_ip, _target_port))
+
+        try:
+            message_rx, address = rx_tx_socket.recvfrom(_buffer_size)
+            print(f"rx ({address[0]}:{address[1]}): {message_rx.decode()}")
+        except TimeoutError:
+            print(f"Timeout ({DEFAULT_TIMEOUT} s)!")
 
 
 if __name__ == "__main__":
-    main()
+    local_port, target_ip, target_port, buffer_size = read_cmd_arguments()
+    main(local_port, target_ip, target_port, buffer_size)
--- udp_server.py
+++ udp_server.py
@@ -1,31 +1,48 @@
 #!/usr/bin/env python
 
+import argparse
 import socket
 from simple_message import simple_message_pb2, simple_message_utils
 
-local_ip = '0.0.0.0'
-local_port = 20212
-buffer_size = 1024
+DEFAULT_LOCAL_IP = '0.0.0.0'
+DEFAULT_LOCAL_PORT = 20212
+DEFAULT_BUFFER_SIZE = 1024
 
 
-def main():
-    print("udp_server")
+def read_cmd_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--local-ip", type=str, default=DEFAULT_LOCAL_IP,
+                        help="IP where the local server is going to listen to")
+    parser.add_argument("-p", "--local-port", type=int, default=DEFAULT_LOCAL_PORT,
+                        help="Port where the local server is going to listen to")
+    parser.add_argument("-b", "--buffer-size", type=int, default=DEFAULT_BUFFER_SIZE,
+                        help="Size of the internal rx/tx buffer in bytes")
+    args = parser.parse_args()
+    return args.local_ip, args.local_port, args.buffer_size
 
-    udp_server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-    udp_server_socket.bind((local_ip, local_port))
-    while True:
 
-        message_rx_raw, address = udp_server_socket.recvfrom(buffer_size)
-        message_rx = simple_message_pb2.simple_message()
-        message_rx.ParseFromString(message_rx_raw)
-        message_rx_str = simple_message_utils.simple_message_to_str(message_rx)
-        print(f"rx ({address}): {message_rx_str} (raw: {message_rx_raw})")
+def main(_local_ip: str, _local_port: int, _buffer_size: int) -> None:
+    print(f"udp_server[{_buffer_size}] {_local_ip}:{_local_port}")
 
-        # Sending a reply to client
-        message_tx = "ACK " + message_rx_str
-        udp_server_socket.sendto(str.encode(message_tx), address)
-        print(f"tx ({address}): {message_tx}")
+    with socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM) as udp_server_socket:
+        udp_server_socket.bind((_local_ip, _local_port))
+
+        # try:
+        while True:
+            message_rx_raw, address = udp_server_socket.recvfrom(_buffer_size)
+            message_rx = simple_message_pb2.simple_message()
+            message_rx.ParseFromString(message_rx_raw)
+            message_rx_str = simple_message_utils.simple_message_to_str(message_rx)
+            print(f"rx ({address}): {message_rx_str} (raw: {message_rx_raw})")
+
+            # Sending a reply to client
+            message_tx = "ACK " + message_rx_str
+            udp_server_socket.sendto(str.encode(message_tx), address)
+            print(f"tx ({address}): {message_tx}")
+        # except KeyboardInterrupt:
+        #     print("shutting server")
 
 
 if __name__ == "__main__":
-    main()
+    local_ip, local_port, buffer_size = read_cmd_arguments()
+    main(local_ip, local_port, buffer_size)
